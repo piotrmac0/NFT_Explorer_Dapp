4aa79ecb77e71eeda141abe57336411ee0aa0dd0
Piotr Mac <mac.piotr0@gmail.com>
2023-01-07 16:56:21 +0100

    WorkingApp_update

--- pages/components/nftCard.jsx
+++ pages/components/nftCard.jsx
@@ -1,21 +1,21 @@
 //creating NFT compontent with nft as props
 export const NFTCard = ({nft}) => {
     return (
-        <div className="w-1/4 flex flex-col">
-            <div className="rounded-md">
+        <div className="w-3/4 md:w-1/4 flex flex-col rounded-lg">
+            <div>
                 <img src={nft.media[0].gateway} className="object-cover h-128 w-full rounded-t-lg"></img>
             </div>
-            <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 roundedb-md h-110">
+            <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-lg h-110">
                 <div>
-                    <h2 className="text-gray-800">{nft.title}</h2>
+                    <h2 className="text-gray-800 font-bold">{nft.title}</h2>
                     <p className="text-gray-600">{nft.id.tokenId.substr(nft.id.tokenId.length -4)}</p>
                     <p className="text-gray-600">{`${nft.contract.address.substr(0,5)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`} </p>
+                    <div className="flex justify-center mb-1">
+                        <a href={`https://etherscan.io/token/${nft.contract.address}`} target={"_blank"} className="py-2 px-4 bg-blue-500 w-3/5 md:w-5/5 text-center rounded-lg text-white cursor-pointer mt-3">See on Etherscan</a>
+                    </div>
                 </div>
             </div>
            
-            <div className="flex justify-center mb-1">
-                <a href={`https://etherscan.io/token/${nft.contract.address}`} target={"_blank"} className="py-2 px-4 bg-blue-500 w-1/2 text-center rounded-lg text-white cursor-pointer">See on Etherscan</a>
-            </div>
         </div>
     )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -29,7 +29,7 @@ const Home = () => {
           };
           // Replace with the wallet address you want to query:
           //const ownerAddr = "0xF5FFF32CF83A1A614e15F25Ce55B0c0A6b5F8F2c";
-          const fetchURL = `${baseURL}?owner=${wallet}`;
+          const fetchURL = `${baseURL}?owner=${wallet}&orderBy=transferTime`;
           //fetch function and return json format
           nfts = await fetch(fetchURL, requestOptions).then(data => data.json());
       } 
@@ -37,7 +37,7 @@ const Home = () => {
       else { 
           console.log("Fetching nfts for collection owned by address");
           //fetch nfts belonging to wallet and collection both
-          const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}`;
+          const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}&orderBy=transferTime`;
           nfts = await fetch(fetchURL, requestOptions).then(data => data.json());
 
         }
@@ -54,8 +54,8 @@ const Home = () => {
   const fetchNFTsForCollection = async () => {
     if (collection.lenght) {
       const apiKey = "2w9FFX8SC_ugaKEFXHRb-nEu1Dmcs82v";
-      const baseURL = `https://eth-mainnet.g.alchemy.io/nft/v2/${apiKey}/getNFTsForCollection/`;
-      const fetchURL = `${baseURL}?contractAddress=${wallet}&withMetadata=${true}`;
+      const baseURL = `https://eth-mainnet.g.alchemy.com/nft/v2/${apiKey}/getNFTsForCollection/`;
+      const fetchURL = `${baseURL}?contractAddress=${wallet}&withMetadata=${true}&orderBy=transferTime`;
      // const baseURL = `https://eth-mainnet.alchemyapi.io/nft/v2/${apiKey}/getNFTsForCollection`;
       //const url = `${baseURL}?contractAddress=${address}&withMetadata=${withMetadata}`;
       var requestOptions = {
@@ -73,20 +73,21 @@ const Home = () => {
 
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
-      <div className="flex flex-col w-full justify-center items-center gap-y-2">
+      <h1 className="flex justify-center font-bold mb-10 mt-5 text-3xl leading-none text-slate-700">NFT Wallets Explorer</h1>
+      <div className="flex flex-col w-full justify-center items-center gap-y-3">
         {/*onChange for useSate, e = event, */}
-        <input disabled={fetchForCollection} className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
-        onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="Add your wallet address"> 
+        <input disabled={fetchForCollection} className="w-4/5 md:w-3/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
+        onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="Add the wallet address"> 
         </input>
 
-        <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
+        <input className="w-4/5 md:w-3/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
         onChange={(e)=>{setCollectionAddress(e.target.value)}} value={collection} type={"text"} placeholder="Add the collection address">
         </input>
 
-        <label className="text-gray-600"><input onChange={(e)=>{setFetchForCollection(e.target.checked)}} type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
+        <label className="text-gray-600"><input onChange={(e)=>{setFetchForCollection(e.target.checked)}} type={"checkbox"} className="mr-2"></input>Fetch only collection</label>
         {/*button with fetchings its collection or not and calling proper to that state function */}
         
-        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"}
+        <button className={"disabled:bg-slate-500 text-white bg-slate-800 px-4 py-2 mt-3 rounded-md w-2/5 md:w-1/5 mb-10"}
         onClick={() => {
             if(fetchForCollection) {
                 fetchNFTsForCollection()
@@ -95,7 +96,7 @@ const Home = () => {
         }>Search NFTs</button>
       </div>
 
-      <div className="flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center">
+      <div className="flex flex-wrap gap-y-12 mt-4 w-6/6 gap-x-5 justify-center">
         {NFTs.length && NFTs.map(nft => {
           return (
             <NFTCard nft={nft}></NFTCard>
